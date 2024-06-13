8231ecfbbedf03505de013699a494de4e86fa47f
csdavids519 <csdavids@gmail.com>
2024-06-13 13:05:23 +0200

    Add player win check - in progress

--- run.py
+++ run.py
@@ -1,9 +1,10 @@
 
-grid_row_size = 7
-grid_column_size = 6
+grid_row_size = 6
+grid_column_size = 7
 grid = [["[_]" for c in range(grid_column_size)] for r in range(grid_row_size)]  
 
-token_positions = [[]]
+token_position_row = 0
+token_position_col = 0
 
 input_column = 0
 
@@ -14,13 +15,9 @@ def test():
     grid[4][4] = " o "
     grid[4][3] = " o "
     
+    print(grid)
     
     
-    
-
-
-
-
 def player_input():
     """
     Check payer input value is int and in rage of the game grid
@@ -42,16 +39,20 @@ def player_input():
         else:
             print("Enter a value between 0 and 6")
             continue
-    # player_input()
 
 def check_place_position():
     """
     Check each row in the users selected column starting at the bottom, find the first empty position
     """
+    global token_position_row
+    global token_position_col
+
     for i in range(5, -1, -1):
         if grid[int(input_column)][i] == "[_]":
             print(f" {i} is blank")  #debug
             grid[int(input_column)][i] = " X "
+            token_position_row = i
+            token_position_col = int(input_column)
             break #only place one token
         elif (i == 0) and grid[int(input_column)][i] != "[_]":
             print("this column is full, pick another")
@@ -61,8 +62,8 @@ def update_grid():
     """
     draw grid with updated player move
     """
-    for c in range(grid_column_size):
-        for r in range(grid_row_size):
+    for r in range(grid_column_size):
+        for c in range(grid_row_size):
             print(grid[r][c], end=" ")
         print("")
     check_winner()
@@ -71,10 +72,40 @@ def update_grid():
 def check_winner():
     """
     Check for a game win or change player
+    - code reference Shaun Halverson see readme for link
     """
+    global token_position_row
+    global token_position_col
 
     # check horizontal 
-    
+    for y in range(grid_row_size):
+        for x in range(grid_column_size -3):
+            if (grid[x][y] == " X " and grid[x+1][y] == " X " and grid[x+2][y] == " X " and grid[x+3][y] == " X "):
+                print(f"x winner {x}")
+                return True
+            
+    # check vertical
+    for y in range(grid_row_size):
+        for x in range(grid_column_size -3):
+            if (grid[x][y] == " X " and grid[x][y+1] == " X " and grid[x][y+2] == " X " and grid[x][y+3] == " X "):
+                print(f"x winner {x}")
+                return True
+            
+    # check diagonal top right to bottom left
+    for y in range(grid_row_size -3):
+        for x in range(3, grid_column_size):
+            if (grid[x][y] == " X " and grid[x+1][y-1] == " X " and grid[x+2][y-2] == " X " and grid[x+3][y-3] == " X "):
+                print(f"x winner {x}")
+                return True
+            
+    # check diagonal top left to bottom right
+    for y in range(grid_row_size -3):
+        for x in range(grid_column_size - 3):
+            if (grid[x][y] == " X " and grid[x+1][y+1] == " X " and grid[x+2][y+2] == " X " and grid[x+3][y+3] == " X "):
+                print(f"x winner {x}")
+                return True
+            
+    player_input()
 
 
 
